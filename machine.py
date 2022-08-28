074ae5785bdac1de9b218c093ac8a6f7fc536931
victorsvart <victor.cscience@gmail.com>
2022-08-28 19:36:18 -0300

    First Commit

---
+++ machine.py
@@ -0,0 +1,28 @@
+import random
+import time
+
+
+def machine():
+    x = random.randrange(1, 6)
+    num = int(
+        input(
+            "Try the guess the number the machine will output.\nIf you get it right, you win.\nIf you get wrong, your system32 folder will be deleted :)\nTry to guess: "
+        )
+    )
+    print(f"The machine thought about... {x}!")
+    time.sleep(1)
+    if num == x:
+        print("You WIN! Another day you will have a computer!")
+    else:
+        print("You lose. Deleting system32 folder...")
+        time.sleep(2)
+        print("System32 folder suscessfuly deleted!\nGood Luck!")
+        exit()
+
+
+def menu():
+    print("*" * 10, "Welcome to BEAT THE MACHINE!", "*" * 10)
+
+
+menu()
+machine()
