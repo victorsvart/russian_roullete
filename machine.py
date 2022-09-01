4dc422783cc159ff462822d9ca43d2004341441d
victorsvart <victor.cscience@gmail.com>
2022-08-31 22:37:27 -0300

    Executable File Added

--- machine.py
+++ machine.py
@@ -7,17 +7,19 @@ def machine():
     num = int(
         input(
             "Try the guess the number the machine will output.\nIf you get it right, you win.\nIf you get wrong, your system32 folder will be deleted :)\nTry to guess: "
-        )
-    )
+        ))
     print(f"The machine thought about... {x}!")
     time.sleep(1)
     if num == x:
         print("You WIN! Another day you will have a computer!")
+        print("Goodbye! :)")
+        time.sleep(3)
+
     else:
-        print("You lose. Deleting system32 folder...")
+        print("you lose. Deleting system32 folder...")
         time.sleep(2)
         print("System32 folder suscessfuly deleted!\nGood Luck!")
-        exit()
+        time.sleep(3)
 
 
 def menu():
